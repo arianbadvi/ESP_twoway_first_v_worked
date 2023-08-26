9b4c1a5a2a4400b0bdf0fab3917b401c56c56fb4
arianbadvi <aryan_kare2@yahoo.com>
2023-08-26 16:52:52 +0330

    The two way communication of sending status is added to the code, and it is tested on the hardware.

--- to_way_esp_micro/ESP/ESP_SBM_TWO_WAY/ESP/ESP.ino
+++ to_way_esp_micro/ESP/ESP_SBM_TWO_WAY/ESP/ESP.ino
@@ -1,40 +1,32 @@
 // Program Configs ////////////////////////////////////////////////////////////////////////////////////////////
 #define ESP_chipid                              13755325
-
 // Debug mode
 // ESP send decoded data to its output so serial can monitor by user
 // In debug mode ChipID not check and its value send to serial at begin of code
 #define debug_mode                              true
-
 // WiFi Station User and Password
 #define WiFi_SSID                               "two_way10"
 // !! Password mast be at least 8 chars !!
 #define WiFi_password                            "12341235"
-
 // WiFi channel (1-11)
 #define WiFi_channel                            5
 // WiFi SSID hidden: network name will not be shown when this option is true
 #define WiFi_hidden                             false
 // Spcify number of users can be connected  to the ESP access point
 #define WiFi_users_limit                        1
-
 #define IP_local                                192,168,4,2
 #define IP_gateway                              192,168,4,1
 #define IP_subnet                               255,255,255,0          
 #define UDP_port                                4210
-
 // UDP start command: Computer must send this command to ESP in case of start working
 #define UDP_start_command                       "s"
-
 // battery number of average samples (each sample increase update wait of battery for 10ms)
 #define battery_average_sample_count            50
 // ADC sample accuracy. if diffrence between two sample is more than this number, the last one will remove (2 - 20)
 #define battery_average_sample_accuracy         2
-
 // Baudrate of sending data
 // in ESP this option must be the same
 #define send_data_baudrate                      115200
-
 // decode data that recived from uart
 // Prevent from stealing data
 // in Arduino this option must be the same
@@ -42,42 +34,32 @@
 // in Arduino this option must be reverse
 // formula to encode each char: x is char (between 0 and 255)
 #define decode_formula(x)                       (255 - x)
-
 #define UDP_FIRST   "aaaaaaaa"
 #define UDP_SECOND  "24682468246824682468"
 #define UDP_THIRD   "11110011"
-
+#define STATUS_UDP  "11111111"
+#define MAIN_BOARD_SEND_STATUS_STRING "ssssssss"
 #define SERIAL_FIRST  "1231"
 #define SERIAL_SECOND_V "YES"
 #define SERIAL_SECOND_I "NO"
 
-
-// Library Configs ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <ESP8266WiFi.h>    // ESP8266 Library
 #include <WiFiUDP.h>        // UDP Protocol Library
 
-// Variables /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 // if this variable is one ESP will not send any data
 bool program_lock = true;
-
 // WiFi Station User and Password
 const char *ssid = WiFi_SSID;
 const char *password = WiFi_password; 
-
 // IPv4 variables
 IPAddress local_IP(IP_local);
 IPAddress gateway(IP_gateway);
 IPAddress subnet(IP_subnet);
-
 // UDP connection variables
 WiFiUDP Udp;                            // Create and object of UDP Class
 unsigned int localUdpPort = UDP_port;   // local port to listen on (On computer side remote port is UDP_port)
-
 IPAddress remoteUdpIP;                  // IP of client (Here means computer that sent request to ESP)
 unsigned int remoteUdpPort;             // Port of client
-// IP and port of client are required to send data for client
-
 int replyPacketB_cur = 0;
 char replyPacketB[255];
 char incomingPacket[255];
@@ -126,8 +108,6 @@ void loop()
     {      
       remoteUdpIP = Udp.remoteIP();                                                             // save sender IP for future works
       remoteUdpPort = Udp.remotePort();                                                         // save sender PORT for future works
-      //added for test
-      //for (int i =0; i < 30; i++) incomingPacket[i] = 0;
       int len = Udp.read(incomingPacket, 20);                                                  // read packet value and save in incomingPacket array
       incomingPacket[len] = 0;                                                                  // define end of string      
       yield();
@@ -156,13 +136,10 @@ void loop()
         {
           if (strcmp(incomingPacket, UDP_FIRST)  == 0)
           {
-            yield();
-            
+            yield();        
             for (int i = 0; i < 30; i++)  udp_reset_str[i] = 0;
-            for (int i = 0; i < strlen(UDP_SECOND); i++) udp_reset_str[i] = 'R'; 
-                        
-            Serial.print(udp_reset_str);
-                        
+            for (int i = 0; i < strlen(UDP_SECOND); i++) udp_reset_str[i] = 'R';                     
+            Serial.print(udp_reset_str);          
             udp_state = 0;
             serial_state = 0;
           }
@@ -170,9 +147,10 @@ void loop()
           {
             Serial.print(incomingPacket);
           }
+        }
         break;
-
         case 2:
+        {
           if (strcmp(incomingPacket, UDP_THIRD)  == 0)
           {         
             yield();
@@ -186,7 +164,16 @@ void loop()
             Serial.print(udp_reset_str);
             udp_state = 0;
             serial_state = 0;
-          }                      
+          }     
+          else if(strcmp(incomingPacket, STATUS_UDP) == 0)
+          {
+            yield();
+            serial_state = 4;
+            Serial.print(MAIN_BOARD_SEND_STATUS_STRING);
+            Udp.beginPacket(remoteUdpIP, remoteUdpPort);
+            Udp.write("Sent to micro\n");
+            Udp.endPacket(); 
+          }                 
           else
           {
             yield();
@@ -205,13 +192,8 @@ void loop()
       {
         case 0:
         {
-          for(int i = 0; i<20; i++ )
-          {
-            buffer_m[i] = 0;
-          }
+          for(int i = 0; i<20; i++ )  buffer_m[i] = 0;
           Serial.readBytes(buffer_m, 20);
-          //if (strcmp(buffer_m, SERIAL_FIRST)  == 0)
-          
           if (strcmp(buffer_m, "SERIAL")  == 0)
           {
             Serial.print("OK");
@@ -220,28 +202,19 @@ void loop()
             WiFi.mode(WIFI_OFF);
             delay(1000);                            
           }
-          if(1)
-          {
-            yield();
-            Udp.beginPacket(remoteUdpIP, remoteUdpPort);
-            //Udp.write("bbbb\n");
-            //const char buffer_str[30] = SERIAL_FIRST ;
-            
-            Udp.write( buffer_m );
-            Udp.write("\n");
-            Udp.endPacket();
-            serial_state = 1;
-            udp_state = 1;
-          }
+          yield();
+          Udp.beginPacket(remoteUdpIP, remoteUdpPort); 
+          Udp.write( buffer_m );
+          Udp.write("\n");
+          Udp.endPacket();
+          serial_state = 1;
+          udp_state = 1;
         }
         break;
         case 1:
         {
           yield();
-          for(int i = 0; i<20; i++ )
-          {
-            buffer_m[i] = 0;
-          }
+          for(int i = 0; i<20; i++ )  buffer_m[i] = 0;
           Serial.readBytes(buffer_m, 20);
           if (strcmp(buffer_m, SERIAL_SECOND_V)  == 0)         
           {
@@ -250,12 +223,9 @@ void loop()
             Udp.endPacket();
             serial_state = 3;
             udp_state = 2;
-            //process = 1;
-            //udp_state = 0;
           }
           else if (strcmp(buffer_m, SERIAL_SECOND_I)  == 0)
           {
-            //string_buffer = String(buffer_m) + "\n";
             Udp.beginPacket(remoteUdpIP, remoteUdpPort);
             Udp.write("NO\n");
             Udp.endPacket();
@@ -275,24 +245,26 @@ void loop()
         case 3:
         {
           yield();
-          for(int i = 0; i<20; i++ )
-          {
-            buffer_m[i] = 0;
-          }
-          Serial.readBytes(buffer_m, 20);
-          
+          for(int i = 0; i<20; i++ )  buffer_m[i] = 0;
+          Serial.readBytes(buffer_m, 20);      
           Udp.beginPacket(remoteUdpIP, remoteUdpPort);  
           Udp.write( buffer_m );
           Udp.write("\n");
           Udp.endPacket();
-          //Udp.beginPacket(remoteUdpIP, remoteUdpPort);
-          //Udp.write("YES\n");
-          //Udp.endPacket();
-          //serial_state = 3;
-          //udp_state = 2;
           process = 1;
           udp_state = 0;
           serial_state = 0;
+        }
+        break;
+        case 4:
+        {
+          for(int i = 0; i<20; i++ )  buffer_m[i] = 0;
+          Serial.readBytes(buffer_m, 9);      
+          Udp.beginPacket(remoteUdpIP, remoteUdpPort);  
+          Udp.write(buffer_m);
+          Udp.write("\n");
+          Udp.endPacket();
+          serial_state = 3;
         }
         break;
       }
